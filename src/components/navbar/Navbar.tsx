43931b6b83b15483fd328c61223d48729c5f8750
Noparu <noparuuu@gmail.com>
2024-08-12 11:38:55 +0700

    rebuild web

---
+++ src/components/navbar/Navbar.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Navbar = () => {
+    return (
+        <div className="w-full flex items-center justify-between px-[5vh] py-[2vh]">
+            <div className="">
+                💀
+            </div>
+            <div className="">
+                💀
+            </div>
+        </div>
+    )
+}
+
+export default Navbar
